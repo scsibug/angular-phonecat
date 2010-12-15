d65dd27e93fba35306720848a2befb6da4fd16e9
Misko Hevery <misko@hevery.com>
2010-12-15 10:04:27 -0800

    added checked filter

---
+++ app/js/filters.js
@@ -0,0 +1,3 @@
+angular.filter('checked', function(value){
+  return value ? "\u2714" : "\u2716";
+});
---
+++ test/unit/filterSpec.js
@@ -0,0 +1,14 @@
+describe('filter', function(){
+  describe('checked', function(){
+    it('should show check on true', function(){
+      expect(angular.filter('checked')(true)).toEqual("\u2714");
+    });
+
+    it('should show cross on false', function(){
+      expect(angular.filter('checked')(undefined)).toEqual("\u2716");
+    });
+    it('should show check on undefined', function(){
+      expect(angular.filter('checked')(false)).toEqual("\u2716");
+    });
+  });
+});
